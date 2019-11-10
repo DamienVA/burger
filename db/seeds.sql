8c686bd98e71ecb8172fb129276f5eb86a76f373
Damien Aponte <damienapo@gmail.com>
2019-11-10 18:09:24 -0500

    created burger database and table. Table filled with seed info.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Rodeo BBQ');
+
+INSERT INTO burgers (burger_name) VALUES ('Mushroom & Swiss');
+
+INSERT INTO burgers (burger_name) VALUES ('Bluecheese Kobe');
